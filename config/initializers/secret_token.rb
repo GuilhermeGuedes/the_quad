912b80c6f12c144fe8e8400945433c50d28f9898
William Guedes <wguedes@wguedes-VirtualBox.(none)>
2012-09-08 09:09:43 -0400

    First commit - Just ran a rails new the_quad, Nothing else. No Edits done.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheQuad::Application.config.secret_token = 'aabf99649e7efa909894f22d15829c43657c47957545b68923cd244478a8b61e731d3abaf36bef7c3aa62ad9cc2462e3ba31f789b8bc8eea2c2c3e0f079df92d'
